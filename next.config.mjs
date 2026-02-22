e2b8222afc6f5910143eefce720dd28cc7647f40
Christopher Bruce  Perkins <cons_CHP@jysk.com>
2026-02-22 22:36:42 +0100

    testing base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: false,
+  // optional: enable image domains, etc.
+}
 
 export default nextConfig;
